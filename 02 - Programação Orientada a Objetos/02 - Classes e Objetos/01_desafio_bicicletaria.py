fbb1760478c4df6bf4b00a238e14ce04c0829c9d
julia.machado <julia.machado@mngs.com.br>
2024-09-12 08:34:26 -0300

    add anotacoes poo

--- 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
+++ 02 - Programação Orientada a Objetos/02 - Classes e Objetos/01_desafio_bicicletaria.py
@@ -1,4 +1,7 @@
 class Bicicleta:
+    #FUNÇÃO INICIALIZADORA/CONSTRUTORA EM CLASSES 
+
+    #SELF É UMA REFERENCIA/INSTANCIA DO OBJETO
     def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo
@@ -15,11 +18,14 @@ class Bicicleta:
     def correr(self):
         print("Vrummmmm...")
 
+#metodo para retornar os valores dos atributos
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 
 b1 = Bicicleta("vermelha", "caloi", 2022, 600)
+
+print(b1)
 b1.buzinar()
 b1.correr()
 b1.parar()
