c412d67cd5ffa01ab0470a0f076d779db69feb63
julia.machado <julia.machado@mngs.com.br>
2024-09-12 10:50:27 -0300

    anotacoes heranca multipla e MRO

--- 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
+++ 02 - Programação Orientada a Objetos/04 - Herança/02_heranca_multipla.py
@@ -5,17 +5,25 @@ class Animal:
     def __str__(self):
         return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
-
+# O **kw é usado para aceitar argumentos nomeados (keyword arguments) adicionais na função/método. 
+# Em Python, o **kw (ou qualquer outro nome de variável após **) coleta todos os argumentos passados por nome e os transforma em um dicionário.
+# Quando o argumento é um keyword arguments, deve ser passado de forma nomeada
 class Mamifero(Animal):
     def __init__(self, cor_pelo, **kw):
         self.cor_pelo = cor_pelo
         super().__init__(**kw)
+    
+    def __str__(self):
+        return 'Mamifero'
 
 
 class Ave(Animal):
     def __init__(self, cor_bico, **kw):
         self.cor_bico = cor_bico
         super().__init__(**kw)
+
+    def __str__(self):
+        return 'Ave'
 
 
 class Gato(Mamifero):
@@ -25,6 +33,13 @@ class Gato(Mamifero):
 class Ornitorrinco(Mamifero, Ave):
     def __init__(self, cor_bico, cor_pelo, nro_patas):
         super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+        #Method Resolution Order é a ordem na qual Python busca métodos e atributos em uma hierarquia de classes.
+        #Nesse exemplo, irá executar o método __str__ de acordo como for encontrado na hierarquia.
+        print(Ornitorrinco.mro())
+
+    def __str__(self):
+        return 'Ornitorrinco'
 
 
 gato = Gato(nro_patas=4, cor_pelo="Preto")
